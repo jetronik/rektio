5239f73c0a1e841a5d1f8ceaa3c098175e2c0a01
Yevheniy <zhenia.dz@gmail.com>
2025-06-24 16:22:05 +0300

    Build 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,58 +2,67 @@ import React, { useState } from "react";
 import Table from "react-bootstrap/Table";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
+
 import InputGroup from "react-bootstrap/InputGroup";
 import { data } from "./data";
 import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   const [search, setSearch] = useState("");
-  //console.log("hello\nworld");
+  const pass = () => {};
 
   return (
     <div className="App">
-      <Container>
-        <h1 className="text-center mt-8">Rektio</h1>
+      <NavBar />
+      <Container className=" pt-5">
         <Form>
+          <br />
+          <h3 className="text-center">Rektio</h3>
+          <lable>Rektio</lable>
           <InputGroup className="my-3">
             <Form.Control
               onChange={(e) => setSearch(e.target.value)}
-              placeholder="Search contacts"
+              placeholder="Etsi sana..."
             />
           </InputGroup>
         </Form>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Sana</th>
-              <th>Rektio</th>
-              <th>Esimerki</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data
-              .filter((item) => {
-                return search.toLocaleLowerCase() === ""
-                  ? item
-                  : item.word.toLocaleLowerCase().includes(search);
-              })
-              .map((item, index) => (
-                <tr key={index}>
-                  <td>{item.word}</td>
-                  <td>
-                    {item.rule.map((value) => (
-                      <p>{value}</p>
-                    ))}
-                  </td>
-                  <td>
-                    {item.rest.map((value) => (
-                      <p>{value}</p>
-                    ))}
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </Table>
+        <div className="my-table">
+          <Table striped bordered hover className="h-3">
+            <thead>
+              <tr>
+                <th>Sana</th>
+                <th>Rektio</th>
+                <th>Esimerki</th>
+              </tr>
+            </thead>
+            <tbody>
+              {search.length > 1
+                ? data
+                    .filter((item) => {
+                      return search.toLocaleLowerCase() === ""
+                        ? item
+                        : item.word.toLocaleLowerCase().includes(search);
+                    })
+                    .map((item, index) => (
+                      <tr key={index}>
+                        <td>{item.word}</td>
+                        <td>
+                          {item.rule.map((value) => (
+                            <p>{value}</p>
+                          ))}
+                        </td>
+                        <td>
+                          {item.rest.map((value) => (
+                            <p>{value}</p>
+                          ))}
+                        </td>
+                      </tr>
+                    ))
+                : pass()}
+            </tbody>
+          </Table>
+        </div>
       </Container>
     </div>
   );
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-md bg-dark navbar-dark py-3 fixed-top">
+      <div className="container">
+        <a href="#" className="navbar-brand">
+          Frontend Bootcamp
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navmenu"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a href="#learn" className="nav-link">
+                What You'll Lern
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#questions" className="nav-link">
+                Queations
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#instructors" className="nav-link">
+                Instuctors
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
